351d395a4223eb510c5aa6b67fb1df6bb1b096de
dailinzhi <dailinzhi2012@163.com>
2024-12-23 17:18:20 +0800

    2024年12月23日17:18:13

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -4,16 +4,24 @@
 @Time:      2024/12/23  15:02
 """
 
-from datetime import datetime
+from datetime import datetime, timedelta
+
+data = {
+
+}
 
 
 def pytest_configure():
-
     # 配置加载完毕之后执行，所有测试用例执行前执行
+    data["start_time"] = datetime.now()
     print(f"{datetime.now()}  pytest开始执行")
 
 
 def pytest_unconfigure():
-
     # 配置卸载之后执行，所有测试用例执行完毕之后执行
+    data["end_time"] = datetime.now()
     print(f"{datetime.now()}  pytest结束执行")
+
+    data["duration"] = data["end_time"] - data["start_time"]
+    print(f"测试用例执行时间：{data['duration']}")
+    assert timedelta(seconds=3) > data["duration"] >= timedelta(seconds=2.5)
---
+++ tests/test_abc.py
@@ -0,0 +1,17 @@
+"""
+@Filename:  test_abc
+@Author:    szkingdom-11
+@Time:      2024/12/23  17:11
+"""
+
+
+import time
+
+def test_api():
+    time.sleep(2.5)
+
+def test_pass():
+    pass
+
+def test_fail():
+    pass
