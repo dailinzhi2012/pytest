ec0d1a0dcb0ed6e39bdd91484edf78586de7e903
dailinzhi <dailinzhi2012@163.com>
2024-12-23 17:59:04 +0800

    2024年12月23日17:58:58

--- src/pytest_result_sender/plugin.py
+++ src/pytest_result_sender/plugin.py
@@ -3,13 +3,17 @@
 @Author:    szkingdom-11
 @Time:      2024/12/23  15:02
 """
-
+import pytest
 from datetime import datetime, timedelta
 
 data = {
 
 }
 
+def pytest_collection_finish(session: pytest.Session):
+    # 收集测试用例结束之后执行，所有测试用例收集完毕之后执行
+    data["total"] = len(session.items)
+    print(f"总共收集到{data['total']}个测试用例")
 
 def pytest_configure():
     # 配置加载完毕之后执行，所有测试用例执行前执行
@@ -24,4 +28,6 @@ def pytest_unconfigure():
 
     data["duration"] = data["end_time"] - data["start_time"]
     print(f"测试用例执行时间：{data['duration']}")
-    assert timedelta(seconds=3) > data["duration"] >= timedelta(seconds=2.5)
+    # assert timedelta(seconds=3) > data["duration"] >= timedelta(seconds=2.5)
+    assert data['total'] == 3
+
